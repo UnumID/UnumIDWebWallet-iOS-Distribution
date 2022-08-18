3b0e1159d14b3821be702d213b02ef4b9292e291
lukaszdw <lucas@unumid.co>
2022-08-18 20:54:19 +0200

    Added UnumIDWebWallet.xcframework

---
+++ UnumIDWebWallet.xcframework/ios-arm64_x86_64-simulator/UnumIDWebWallet.framework/Headers/UnumIDWebWallet.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebAuth.
+FOUNDATION_EXPORT double WebAuthVersionNumber;
+
+//! Project version string for WebAuth.
+FOUNDATION_EXPORT const unsigned char WebAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebAuth/PublicHeader.h>
+
+
